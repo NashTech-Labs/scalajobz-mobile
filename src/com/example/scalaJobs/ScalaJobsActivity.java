efd554638398ddbce51bdfb14a8280f8264e48d3
Ruchi <ruchi@ruchi-Vostro-1550.(none)>
2013-01-26 12:36:23 +0530

    Initial commit

---
+++ src/com/example/scalaJobs/ScalaJobsActivity.java
@@ -0,0 +1,32 @@
+package com.example.scalaJobs;
+
+import org.apache.cordova.DroidGap;
+import org.apache.cordova.*;
+import com.example.scalaJobs.R;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+public class ScalaJobsActivity extends DroidGap {
+
+    @Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/scr.html");
+        
+        // set scrollbar style
+       
+//        setContentView(R.layout.activity_main);
+//        TextView tv_View = (TextView)findViewById(R.id.editText1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+}
